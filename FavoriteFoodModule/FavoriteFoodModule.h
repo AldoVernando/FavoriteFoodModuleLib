057002466b59ba1d04dd7742d43f14b91c23e699
aldo.vernando <aldo.vernando@gmail.com>
2021-02-15 18:17:47 +0700

    Initial Commit

---
+++ FavoriteFoodModule/FavoriteFoodModule.h
@@ -0,0 +1,18 @@
+//
+//  FavoriteFoodModule.h
+//  FavoriteFoodModule
+//
+//  Created by aldo vernando on 15/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FavoriteFoodModule.
+FOUNDATION_EXPORT double FavoriteFoodModuleVersionNumber;
+
+//! Project version string for FavoriteFoodModule.
+FOUNDATION_EXPORT const unsigned char FavoriteFoodModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FavoriteFoodModule/PublicHeader.h>
+
+
