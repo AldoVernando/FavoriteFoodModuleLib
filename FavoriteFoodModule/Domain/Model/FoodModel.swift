13c295fd4e8a3452e0d2cd02f6714c9205140781
aldo.vernando <aldo.vernando@gmail.com>
2021-02-15 18:46:52 +0700

    add model

--- FavoriteFoodModule/Domain/Model/FoodModel.swift
+++ FavoriteFoodModule/Domain/Model/FoodModel.swift
@@ -5,15 +5,15 @@
 //  Created by aldo vernando on 15/02/21.
 //
 
-public class FoodModel {
-    public var id: String
-    public var label: String
-    public var nutrients: NutrientModel?
-    public var category: String?
-    public var categoryLabel: String?
-    public var image: String
+class FoodModel {
+    var id: String
+    var label: String
+    var nutrients: NutrientModel?
+    var category: String?
+    var categoryLabel: String?
+    var image: String
     
-    public init(id: String, label: String?, nutrients: NutrientModel?, category: String?, categoryLabel: String? = "", image: String) {
+    init(id: String, label: String?, nutrients: NutrientModel?, category: String?, categoryLabel: String? = "", image: String) {
         self.id = id
         self.label = label ?? ""
         self.nutrients = nutrients
